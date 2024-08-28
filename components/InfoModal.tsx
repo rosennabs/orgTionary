c423e18b9c3d30210e7738ff58dad93c3bb71989
Rose Francis <rosennabs@gmail.com>
2024-08-28 01:03:02 -0600

    Add more analytical details to the infoModal

--- components/InfoModal.tsx
+++ components/InfoModal.tsx
@@ -8,15 +8,34 @@ function InfoModal({toggleModal}) {
 
   return (
     
-    <div className='absolute left-1/2 transform -translate-x-1/2 bg-white rounded-lg border border-gray-300 w-1/2 h-[500px] shadow-lg p-8 z-50'>
+    <div className='absolute left-1/2 transform -translate-x-1/2 bg-white rounded-lg border border-gray-300 w-2/5 h-[300px] shadow-lg py-8 px-14 z-50'>
       <div className='flex'>
         <button className="ml-auto text-2xl" onClick={() => toggleModal()}>
           <RxCross2 />
         </button>
       </div>
       
-        <h1>Analytics</h1>
-        <p>Posted April 24, 2023</p>
+      <h2 className='mb-2'>Word Details</h2>
+      <p className='mb-12'>Posted April 24, 2023</p>
+      
+      <div className='flex gap-16'>
+        <div className='flex flex-col'>
+          <span className='mb-2'>Views</span>
+          <span className='text-2xl'>8400</span>
+        </div>
+        <div className='flex flex-col'>
+          <span className='mb-2'>Saves</span>
+          <span className='text-2xl'>72</span>
+        </div>
+        <div className='flex flex-col'>
+          <span className='mb-2'>Likes</span>
+          <span className='text-2xl'>122</span>
+        </div>
+        <div className='flex flex-col'>
+          <span className='mb-2'>Comments</span>
+          <span className='text-2xl'>50</span>
+        </div>
+      </div>
       </div>
     
    
