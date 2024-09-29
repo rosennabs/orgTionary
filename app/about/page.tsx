689a810668af4405ecad6fbc84d5be7dcd22a1de
Rose Francis <rosennabs@gmail.com>
2024-09-28 23:12:06 -0600

    Update about us page styling

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ function About() {
   const router = useRouter();
 
   return (
-    <div className="absolute w-full max-h-[100vh] inset-0 top-28 overflow-y-auto p-8 bg-white">
+    <div className="absolute w-full max-h-[100vh] inset-0 top-28 overflow-y-auto p-20 bg-white text-gray-700">
     
       <Link className="text-white text-3xl fixed top-20 right-4" href="#" onClick={(e) => {
         e.preventDefault();
@@ -26,7 +26,7 @@ function About() {
 
       </div>
       
-      <div className='m-12'>
+      <div className=''>
         <h1 className='mb-4'>How it works</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a urna orci. Ut laoreet, tortor pulvinar dapibus suscipit, turpis nisl interdum libero, non commodo elit ex vitae magna. Cras et bibendum justo. Integer faucibus eros quam, vel egestas erat lobortis congue. Sed accumsan odio enim, sit amet vehicula ante elementum et. Morbi ullamcorper pharetra convallis. Mauris ac ultricies nisl, vitae fermentum lacus. Pellentesque scelerisque nibh quis turpis rutrum dapibus. Duis ut finibus nibh. Maecenas mattis, massa eu ultrices tincidunt, orci magna eleifend justo, malesuada imperdiet enim elit vehicula ante. Nam et urna viverra, mollis odio sed, maximus leo. Praesent accumsan ipsum elit, fringilla interdum nunc rhoncus et.</p>
       </div>
