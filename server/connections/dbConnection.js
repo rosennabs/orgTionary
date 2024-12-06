a1e1ccb0303e43f01845a87c4d05988d5f054d33
Rose Francis <rosennabs@gmail.com>
2024-12-06 00:04:29 -0700

    Update code with DATABASE connection string

--- server/connections/dbConnection.js
+++ server/connections/dbConnection.js
@@ -1,13 +1,18 @@
-require("dotenv").config(); //run npm install dotenv
+require("dotenv").config({ path: "../.env" }); //  path to point to your root directory
 
 const { Pool } = require("pg"); //run npm install pg
 
+
 const db = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  // host: process.env.DB_HOST,
+  // port: process.env.DB_PORT,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+ 
 });
+
+
 
 module.exports = db;
