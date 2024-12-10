16be37d305c388cdb108504ac6d8691df36ba170
Rose Francis <rosennabs@gmail.com>
2024-12-09 23:30:28 -0700

    Debug contd: url encode database password in .env'

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ function NotFoundPage() {
   return (
     <div>
       <h1>Page not found!</h1>
-      <p>The requested page could not be found.</p>
+      <p>The requested page could not be found. Try again later!</p>
     </div>
   );
 }
